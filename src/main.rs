a521d65c9a68e6e4fe2436a9141ed44fbad31152
Simon Théorêt <simonteoret@hotmail.com>
2025-05-10 21:26:52 -0400

    WIP

--- src/main.rs
+++ src/main.rs
@@ -1,35 +1,41 @@
-use ahash::AHashMap;
-use compact_str::CompactString;
-use std::{any::Any, fs::File};
+use std::any::Any;
+use ahash::HashMap;
+
+mod reader;
 
 fn main() {
     println!("Hello, world!");
 }
 
-#[derive(Debug, Clone, derive_more::Display, derive_more::DerefMut, derive_more::Deref)]
-pub struct DataString(CompactString);
-
-pub type Metadata = AHashMap<&'static str, Box<dyn Any>>;
-
-#[derive(Debug)]
-pub struct Document {
-    text: DataString,
-    metadata: Metadata,
+pub struct Document<T> {
+    inner: T,
+    data: HashMap<String, Box<dyn Any>>,
 }
 
-pub enum DatasetFileType {
-    JSON,
-    JSONL,
-    TXT,
-}
-
-pub struct FileDataset<I>
-where
-    I: Iterator,
-{
-    handle: File,
-    filetype: DatasetFileType,
-    iter: Option<I>,
-}
-
-
+// #[derive(Debug, Clone, derive_more::Display, derive_more::DerefMut, derive_more::Deref)]
+// pub struct DataString(CompactString);
+//
+// pub type Metadata = AHashMap<&'static str, Box<dyn Any>>;
+//
+// #[derive(Debug)]
+// pub struct Document {
+//     text: DataString,
+//     metadata: Metadata,
+// }
+//
+// pub enum DatasetFileType {
+//     JSON,
+//     JSONL,
+//     TXT,
+// }
+//
+// pub struct FileDataset<I>
+// where
+//     I: Iterator,
+// {
+//     handle: File,
+//     filetype: DatasetFileType,
+//     iter: Option<I>,
+// }
+//
+//
