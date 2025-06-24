a04984070ed6d6d12a5d37f8737034691dc8fe52
Simon Théorêt <simonteoret@hotmail.com>
2025-06-24 14:34:40 -0400

    ci(pre-commit): run hooks with cargo test and cargo fmt

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,7 @@
-
 use clap::Parser;
-use datalints::{inner_main, Args};
+use datalints::{Args, inner_main};
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let args = Args::parse();
     inner_main(args)
 }
-
